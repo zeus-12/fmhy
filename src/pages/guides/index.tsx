86cb238fc31729cdb46e3e63eb2afbc36b38db91
Vishnu <vishnuvinod072@gmail.com>
2023-07-28 21:22:46 +0530

    update guides ui, remove unused stuff

--- src/pages/guides/index.tsx
+++ src/pages/guides/index.tsx
@@ -3,7 +3,6 @@ import GuideItem from "@/components/GuideItem";
 import { Input } from "@mantine/core";
 import { FRONTEND_URL } from "@/lib/config";
 import { notSignedInNotification } from "@/components/Notifications";
-import { Search, X } from "lucide-react";
 import { useRouter } from "next/router";
 import { devLog } from "@/lib/utils";
 
@@ -25,27 +24,14 @@ const Guides = ({
   errorMessage?: string;
 }) => {
   const [inputText, setInputText] = useState("");
-  const [showInput, setShowInput] = useState(false);
 
   const inputElement = useRef<HTMLInputElement>(null);
-
-  const router = useRouter();
-  // const { isSignedIn } = useUser();
-  const isSignedIn = false;
-
-  const addGuideHandler = () => {
-    if (!isSignedIn) {
-      notSignedInNotification("You need to be signed in to add a guide!");
-      return;
-    }
-    router.push("/guides/new");
-  };
 
   useEffect(() => {
     if (inputElement.current) {
       inputElement.current?.focus();
     }
-  }, [showInput]);
+  }, []);
 
   let inputHandler: ChangeEventHandler<HTMLInputElement> = (e) => {
     var lowerCase = (e.target as HTMLInputElement).value;
@@ -68,53 +54,23 @@ const Guides = ({
 
   return (
     <div className="sm:p-4 sm:px-8 md:px-12 lg:px-16 md:py-2 lg:py-4 xl:py-6 pt-0 flex-1 flex flex-col mx-auto w-[95vw] max-w-[80rem]">
-      <div
-        className={` px-4 py-2 justify-between flex sm:flex-row ${
-          showInput ? "" : ""
-        }`}
-      >
+      <div className="px-4 py-2">
         <p
           onClick={() => setInputText("")}
-          className="mb-0 inline text-3xl tracking-tighter font-semibold"
+          className="mb-0 inline text-3xl tracking-tighter font-semibold md:text-4xl"
         >
           Guides
         </p>
-        <div className="flex sm:ml-auto items-center sm:pr-4 sm:gap-2">
-          <div className="search flex items-center gap-2">
-            {showInput ? (
-              <>
-                <Input
-                  className="flex sm:w-52"
-                  type="text"
-                  value={inputText}
-                  ref={inputElement}
-                  onChange={inputHandler}
-                  autoFocus
-                  variant="filled"
-                  placeholder="Search Guide"
-                />
-                <div
-                  className="hover:cursor-pointer"
-                  onClick={() => setShowInput(false)}
-                >
-                  <X className="w-6 h-6" />
-                </div>
-              </>
-            ) : (
-              <div
-                className="hover:cursor-pointer plausible-event-name=search-guide"
-                onClick={() => {
-                  setShowInput(true);
-                }}
-              >
-                <Search className="w-5 h-5" />
-              </div>
-            )}
-          </div>
-          {/* <div onClick={addGuideHandler} className="hover:cursor-pointer">
-            <Plus className="w-6 h-6" />
-          </div> */}
-        </div>
+        <Input
+          className="mt-3 max-w-sm"
+          type="text"
+          value={inputText}
+          ref={inputElement}
+          onChange={inputHandler}
+          autoFocus
+          variant="filled"
+          placeholder="Search Guide"
+        />
       </div>
       {isError && <p>Can&apos;t connect to the server</p>}
 
@@ -122,7 +78,7 @@ const Guides = ({
         <div className="w-full">
           {guides.length > 0 &&
             (filterData(guides)?.length === 0 ? (
-              <p className="mt-2">No results match the entered query</p>
+              <p className="mt-2 px-4">No results match the entered query</p>
             ) : (
               filterData(guides)?.map((item) => (
                 <GuideItem key={item.id} data={item} />
