bd8d4e6ba929d3028d20740d9f15157218f8816f
Vishnu <vishnuvinod072@gmail.com>
2023-08-13 15:25:48 +0530

    eeeeh

--- next.config.mjs
+++ next.config.mjs
@@ -25,5 +25,5 @@ const nextConfig = {
 export default withPlausibleProxy({
   subdirectory: "analytics",
   scriptName: "script",
-  customDomain: "https://a.bignutty.xyz",
+  customDomain: "https://i-totally-love-easylist.swmg.top",
 })(nextConfig);
