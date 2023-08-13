d8f87698752dc8b5c866ef50f6d578e5b8749939
Vishnu <vishnuvinod072@gmail.com>
2023-08-13 14:39:13 +0530

    fmt

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth scroll-mt-6">
       <Head />
-      <code><a rel="me" href="https://very.bignutty.xyz/@FMHY"></a></code>      
+      <code>
+        <a rel="me" href="https://very.bignutty.xyz/@FMHY"></a>
+      </code>
       <body>
         <Main />
         <NextScript />
