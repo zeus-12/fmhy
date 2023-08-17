0d07b37a78b8091315d4c1b1e8250e110f18bd41
Vishnu <vishnuvinod072@gmail.com>
2023-08-18 02:06:41 +0530

    remove guides.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import "@/styles/globals.css";
-import "@/styles/guides.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
