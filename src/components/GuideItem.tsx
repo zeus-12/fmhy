f9566009dc041394c276920513f894c99dab1428
Vishnu <vishnuvinod072@gmail.com>
2023-08-24 09:36:29 +0530

    remove guides

--- src/components/GuideItem.tsx
+++ src/components/GuideItem.tsx
@@ -1,5 +1,5 @@
 import { Badge } from "@mantine/core";
-import type { GuideType } from "@/pages/guides";
+import type { GuideType } from "@/pages/oldguides";
 
 const GuideItem: React.FC<{ data: GuideType }> = ({ data }) => {
   return (
