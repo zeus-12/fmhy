dc530eab58d136dc9a8b74674bd7649ada4c38b4
Vishnu <vishnuvinod072@gmail.com>
2023-09-19 01:20:19 +0530

    remove empty links

--- src/scraper/wiki/db.ts
+++ src/scraper/wiki/db.ts
@@ -2,15 +2,18 @@ import { LinkType } from "../utils/types";
 import prisma from "@/lib/prisma";
 
 export const addLinksToDb = async (data_: LinkType[]) => {
+  console.log("ading duh links");
   await prisma.wiki.createMany({
-    data: data_.map((item) => {
-      return {
-        title: item.title,
-        starred: item.starred,
-        link: JSON.stringify(item.link),
-        nsfw: item.isNsfw,
-      };
-    }),
+    data: data_
+      .filter((item) => item?.link?.length > 0)
+      .map((item) => {
+        return {
+          title: item.title,
+          starred: item.starred,
+          link: JSON.stringify(item.link),
+          nsfw: item.isNsfw,
+        };
+      }),
   });
 };
 
