4537e6a0faa43066ff4916b25055f4ab551452b4
Vishnu <vishnuvinod072@gmail.com>
2023-10-06 22:30:30 +0530

    format data

--- src/pages/lsearch.tsx
+++ src/pages/lsearch.tsx
@@ -5,15 +5,23 @@ import { Input } from "@mantine/core";
 import { SearchIcon } from "lucide-react";
 import ReactMarkdown from "react-markdown";
 
+interface SearchData {
+  results: string[];
+  matchingSections: string[];
+}
+
 const Lsearch = () => {
   const [query, setQuery] = useState("");
   const [debouncedQuery] = useDebouncedValue(query, 200);
-  const [results, setResults] = useState<string[]>([]);
+  const [data, setData] = useState<SearchData>();
   useEffect(() => {
     const searchLocal = async () => {
       const { results, matchingSections } = await doASearch(query);
-      console.log(results, matchingSections);
-      setResults(results);
+      // console.log(results, matchingSections);
+      setData({
+        results,
+        matchingSections,
+      });
     };
 
     searchLocal();
@@ -34,15 +42,27 @@ const Lsearch = () => {
         className="w-[85vw] sm:w-96"
       />
 
-      {debouncedQuery !== query
-        ? "loading.."
-        : query === ""
-        ? "Search for something"
-        : results.map((result, idx) => (
+      {debouncedQuery !== query ? (
+        "loading.."
+      ) : query === "" ? (
+        "Search for something"
+      ) : data && data.results.length + data.matchingSections.length > 0 ? (
+        <>
+          <p>{data.results.length} Results found!</p>
+          {data?.results.map((result, idx) => (
             <div key={idx}>
               <ReactMarkdown>{result}</ReactMarkdown>
             </div>
           ))}
+
+          {/* <p>Matching categories</p>
+          {data.matchingSections.map((section, idx) => (
+            <ReactMarkdown key={idx}>{section}</ReactMarkdown>
+          ))} */}
+        </>
+      ) : (
+        <p> No results found</p>
+      )}
     </div>
   );
 };
