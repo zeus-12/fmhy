94eaac7fc537220e18737607d797faa27261352b
Vishnu <vishnuvinod072@gmail.com>
2023-10-08 10:06:29 +0530

    add /local for testing flexsearch

--- src/pages/local.tsx
+++ src/pages/local.tsx
@@ -3,21 +3,26 @@ import ReactMarkdown from "react-markdown";
 import { useDebouncedValue } from "@mantine/hooks";
 import { Index } from "flexsearch";
 import WikiData from "@/scraper/wiki-v2/data.json";
+import { Input } from "@mantine/core";
 const data = WikiData.data.split("\n");
 
-const LocalSearch = ({ query }: { query: string }) => {
+const LocalSearch = () => {
+  const [query, setQuery] = useState("");
   const [debouncedQuery] = useDebouncedValue(query, 200);
 
   const [index, setIndex] = useState(
     new Index({
       tokenize: "full",
       language: "en",
-      preset: "match",
+      preset: "score",
       cache: true,
-      context: true,
+
+      // context: true,
     })
   );
 
+  // fix the defeault limit of 100 reuslts
+  // find the best parameters for index
   const [results, setResults] = useState<any[]>();
 
   useEffect(() => {
@@ -35,14 +40,23 @@ const LocalSearch = ({ query }: { query: string }) => {
   console.log(results);
 
   return (
-    <p>
-      using flexsearch
-      <span>{results?.length} results </span>
-      {finalResult?.map((result: any, idx) => (
-        // @ts-ignore
-        <ReactMarkdown key={idx}>{result}</ReactMarkdown>
-      ))}
-    </p>
+    <div>
+      <Input
+        placeholder="Try Adblocker"
+        value={query}
+        onChange={(e: any) => setQuery(e.target.value)}
+        className="w-[85vw] sm:w-96"
+      />
+      <div>
+        <p>using flexsearch</p>
+
+        <p>{results?.length} results </p>
+        {finalResult?.map((result: any, idx) => (
+          // @ts-ignore
+          <ReactMarkdown key={idx}>{result}</ReactMarkdown>
+        ))}
+      </div>
+    </div>
   );
 };
 
