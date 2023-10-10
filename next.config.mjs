fbea60d87bc53e588ab7115cfa2fe455d0279941
Vishnu <vishnuvinod072@gmail.com>
2023-10-11 00:19:13 +0530

    update nextconf

--- next.config.mjs
+++ next.config.mjs
@@ -3,27 +3,25 @@ import { withPlausibleProxy } from "next-plausible";
 
 const nextConfig = {
   reactStrictMode: false,
-  // images: {
-  //   domains: ["images.clerk.dev"],
-  // },
-  async redirects() {
+  async rewrites() {
     return [
       {
         source: "/analytics/js/script.js",
         destination: "https://i-totally-love-easylist.swmg.top/js/script.js",
-        permanent: true,
       },
       {
         source: "/analytics/js/script.local.js",
         destination:
           "https://i-totally-love-easylist.swmg.top/js/script.local.js",
-        permanent: true,
       },
       {
         source: "/analytics/api/event",
         destination: "https://i-totally-love-easylist.swmg.top/api/event",
-        permanent: true,
       },
+    ];
+  },
+  async redirects() {
+    return [
       {
         source: "/wiki/:path*",
         destination: "/:path*",
@@ -37,7 +35,7 @@ const nextConfig = {
     ];
   },
 };
-// export default nextConfig;
+
 export default withPlausibleProxy({
   subdirectory: "analytics",
   scriptName: "script",
