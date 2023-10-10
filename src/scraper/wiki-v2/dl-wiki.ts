8e5953089d2a4d9f5bc28f4a0c9652bb2f1ec394
Vishnu <vishnuvinod072@gmail.com>
2023-10-11 01:02:57 +0530

    remove unused

--- src/scraper/wiki-v2/dl-wiki.ts
+++ src/scraper/wiki-v2/dl-wiki.ts
@@ -103,19 +103,8 @@ async function dlWikiChunk(
     }
 
     return items;
-
-    // const updatedLines = addPretext(lines, preText);
-
-    // return updatedLines.join("\n");
   } catch (err: any) {
     console.log("Error fetching data", err.message);
     return [];
   }
-}
-
-function addPretext(lines: string[], preText: string): string[] {
-  for (let i = 0; i < lines.length; i++) {
-    lines[i] = preText + lines[i];
-  }
-  return lines;
 }
