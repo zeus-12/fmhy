fd717cc8f06f8977494fe7dbdee932626a6f4c11
Vishnu <vishnuvinod072@gmail.com>
2023-10-10 22:45:48 +0530

    readd plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { SpotlightProvider } from "@/components/Spotlight";
 import { Notifications } from "@mantine/notifications";
 import Navbar from "@/components/Navbar";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import PlausibleProvider from "next-plausible";
+import PlausibleProvider from "next-plausible";
 import { DefaultSeo } from "next-seo";
 import { cn } from "@/lib/utils";
 import { fontSans } from "@/lib/fonts";
@@ -16,40 +16,40 @@ export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
-    // <PlausibleProvider
-    //   domain="fmhy.net"
-    //   selfHosted={true}
-    //   customDomain="https://i-totally-love-easylist.swmg.top"
-    //   trackOutboundLinks={true}
-    //   taggedEvents={true}
-    //   // trackLocalhost={true}
-    //   // enabled={true}
-    // >
-    <MantineProvider
-      theme={{
-        colorScheme: "dark",
-      }}
+    <PlausibleProvider
+      domain="fmhy.net"
+      selfHosted={true}
+      customDomain="https://i-totally-love-easylist.swmg.top"
+      trackOutboundLinks={true}
+      taggedEvents={true}
+      // trackLocalhost={true}
+      // enabled={true}
     >
-      <QueryClientProvider client={queryClient}>
-        <SpotlightProvider>
-          <DefaultSeo {...SEO} />
+      <MantineProvider
+        theme={{
+          colorScheme: "dark",
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <SpotlightProvider>
+            <DefaultSeo {...SEO} />
 
-          <div
-            className={cn(
-              "antialiased flex flex-col h-screen gap-2 max-w-[100vw] ",
-              fontSans.className
-            )}
-          >
-            <Notifications />
-            <Navbar />
-            <div className="flex-1 flex overflow-y-scroll">
-              <Component {...pageProps} />
+            <div
+              className={cn(
+                "antialiased flex flex-col h-screen gap-2 max-w-[100vw] ",
+                fontSans.className
+              )}
+            >
+              <Notifications />
+              <Navbar />
+              <div className="flex-1 flex overflow-y-scroll">
+                <Component {...pageProps} />
+              </div>
             </div>
-          </div>
-        </SpotlightProvider>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
-    </MantineProvider>
-    // </PlausibleProvider>
+          </SpotlightProvider>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </MantineProvider>
+    </PlausibleProvider>
   );
 }
