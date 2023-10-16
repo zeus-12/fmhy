b5a1e0840ac3d1b2a8234456d99bbd7aa5093f04
Vishnu <vishnuvinod072@gmail.com>
2023-10-16 19:04:31 +0530

    delete scraper-v1

--- src/scraper/index.ts
+++
@@ -1,9 +0,0 @@
-import scrapeBase64AndAddToDb from "./base64";
-import scrapeAndAddToDb from "./wiki";
-
-const main = async () => {
-  await scrapeAndAddToDb();
-  await scrapeBase64AndAddToDb();
-};
-
-main().catch((err) => console.error(err.message));
