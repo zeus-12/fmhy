5f0a034faae7c7cbe81905d521fe27b7d4365447
Vishnu <vishnuvinod072@gmail.com>
2023-11-19 02:18:01 +0530

    remove mantine-notifications, react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SpotlightProvider } from "@/components/Spotlight";
-import { Notifications } from "@mantine/notifications";
 import Navbar from "@/components/Navbar";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import PlausibleProvider from "next-plausible";
+// import PlausibleProvider from "next-plausible";
 import { DefaultSeo } from "next-seo";
 import { cn } from "@/lib/utils";
 import { fontSans } from "@/lib/fonts";
 import { SEO } from "../../next-seo.config";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const queryClient = new QueryClient();
-
   return (
     // <PlausibleProvider
     //   domain="fmhy.tk"
@@ -30,26 +25,22 @@ export default function App({ Component, pageProps }: AppProps) {
         colorScheme: "dark",
       }}
     >
-      <QueryClientProvider client={queryClient}>
-        <SpotlightProvider>
-          <DefaultSeo {...SEO} />
+      <SpotlightProvider>
+        <DefaultSeo {...SEO} />
 
-          <Navbar />
+        <Navbar />
 
-          <div
-            className={cn(
-              "antialiased flex flex-col gap-2 hd-screen",
-              fontSans.className
-            )}
-          >
-            <Notifications />
-            <div className="flex-1 flex overflow-y-scroll mt-4">
-              <Component {...pageProps} />
-            </div>
+        <div
+          className={cn(
+            "antialiased flex flex-col gap-2 hd-screen",
+            fontSans.className
+          )}
+        >
+          <div className="flex-1 flex overflow-y-scroll mt-4">
+            <Component {...pageProps} />
           </div>
-        </SpotlightProvider>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+        </div>
+      </SpotlightProvider>
     </MantineProvider>
     // </PlausibleProvider>
   );
