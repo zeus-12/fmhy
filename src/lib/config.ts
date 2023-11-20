dcad74e693d0d3c7ed6c3c5f2363dbc734683cbd
Vishnu <vishnuvinod072@gmail.com>
2023-11-20 21:16:05 +0530

    remove unused

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,1 @@
 const dev = process.env.NODE_ENV !== "production";
-
-export const SERVER_URL = dev
-  ? "http://localhost:8080"
-  : process.env.NEXT_PUBLIC_SERVER_URL;
-
-export const FRONTEND_URL = dev ? "http://localhost:3000" : "https://fmhy.tk";
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,18 +1,6 @@
 import { ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function formatName(name: string) {
-  if (!name) return name;
-  let nameArray = name.replaceAll("-", " ").replaceAll("_", " ").split(" ");
-
-  let formattedName = "";
-  for (let i = 0; i < nameArray.length; i++) {
-    formattedName +=
-      nameArray[i][0].toUpperCase() + nameArray[i].slice(1) + " ";
-  }
-  return formattedName.replaceAll("Ios", "iOS").replaceAll("os", "OS");
-}
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
