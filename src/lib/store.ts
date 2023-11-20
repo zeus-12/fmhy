db3dbc08e8112abcb589b266bbfcf228d58cde6c
Vishnu <vishnuvinod072@gmail.com>
2023-11-20 18:32:17 +0530

    add toc close and toggle to store

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -2,8 +2,10 @@ import { create } from "zustand";
 
 type Wiki = {
   showOnlyStarred: boolean;
-
   toggleWikiToggleStarred: () => void;
+
+  showToc: boolean;
+  toggleShowToc: () => void;
 };
 
 export const useWiki = create<Wiki>((set) => ({
@@ -11,4 +13,7 @@ export const useWiki = create<Wiki>((set) => ({
 
   toggleWikiToggleStarred: () =>
     set((state) => ({ showOnlyStarred: !state.showOnlyStarred })),
+
+  showToc: false,
+  toggleShowToc: () => set((state) => ({ showToc: !state.showToc })),
 }));
