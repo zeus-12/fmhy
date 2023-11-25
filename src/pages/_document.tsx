35cb8e59fba3eb563a7613577c651775ac1f7f33
Vishnu <vishnuvinod072@gmail.com>
2023-11-25 20:43:54 +0530

    remove unused

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,6 @@ export default function Document() {
   return (
     <Html lang="en" className="scroll-smooth scroll-mt-6">
       <Head />
-      <code>
-        <a rel="me" href="https://very.bignutty.xyz/@FMHY"></a>
-      </code>
       <body>
         <Main />
         <NextScript />
